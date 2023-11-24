598a134a29d2895b3db8c25014a39897db642aa3
Actanzil <tanziluadjiganesha@gmail.com>
2023-11-24 16:41:45 +0700

    Memperbarui Database

--- dbconnect.php
+++ dbconnect.php
@@ -1,5 +1,5 @@
 <?php 
-// isi nama host, username mysql, dan password mysql anda
-$conn = mysqli_connect("localhost","root","","db_pupuk");
+
+    $conn = mysqli_connect("localhost","root","","db_pupuk");
 
 ?>
