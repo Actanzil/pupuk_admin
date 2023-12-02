5ba12dc56ca4713f051c1bfbb7f554cbe812403a
Actanzil <tanziluadjiganesha@gmail.com>
2023-12-02 14:13:58 +0700

    clearfix

--- stock/index.php
+++ stock/index.php
@@ -220,19 +220,6 @@
                                         </tbody>
                                     </table>
                                 </div>
-                                
-                                <!-- Pagination -->
-                                <div class="card-footer">
-                                    <nav aria-label="...">
-                                    <ul class="pagination pagination-sm">
-                                        <li class="page-item active" aria-current="page">
-                                        <span class="page-link">1</span>
-                                        </li>
-                                        <li class="page-item"><a class="page-link" href="#">2</a></li>
-                                        <li class="page-item"><a class="page-link" href="#">3</a></li>
-                                    </ul>
-                                    </nav>
-                                </div>
                             </div>
                         </div>
                     </div>
